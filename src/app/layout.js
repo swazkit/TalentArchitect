240c41dc2dd6bb80b0cc55d454f0db3f74de8c35
swazkit <swastikraychoudhuri@gmail.com>
2025-05-01 05:39:56 +0530

    metadata added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'HR Consultancy - Empowering Your Workforce',
-  description: 'Leading HR Consultancy Services Tailored for Modern Businesses',
-}
+  title: 'Talent Architect',
+  description: 'Leading HR Consultancy Services',
+  icons: [
+    { rel: 'icon', url: '/favicon.png', sizes: '32x32', type: 'image/png' },
+    { rel: 'apple-touch-icon', url: '/favicon.png' },
+  ],
+};
+
 
 export default function RootLayout({ children }) {
   return (
